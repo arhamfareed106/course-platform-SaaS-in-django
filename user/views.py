9e0ca38b85597f151aeed95d82c529664e621757
arhamfareed106 <arhamfareed575@gmail.com>
2024-10-15 10:31:26 +0500

    let user

--- user/views.py
+++ user/views.py
@@ -2,18 +2,17 @@ import re
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.contrib.auth import login
-
-# Create your views here.
+from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
-        form= CustomUserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user= form.save()
+            user = form.save()
             login(request, user)
-            return redirect("/course")
+            messages.success(request, "Registration successful. You are now logged in.")
+            return redirect("course")  # Use URL name instead of hardcoding the path
     else:
         form = CustomUserCreationForm()
         
-    return render(request, "registration/register.html", {"form": form})  # Added missing comma
-    
+    return render(request, "registration/register.html", {"form": form})
