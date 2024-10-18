fb1af65abb71d6cd31cc2bd265640cd979055e5d
arhamfareed106 <arhamfareed575@gmail.com>
2024-10-18 23:12:05 +0500

    payment

--- payment/urls.py
+++ payment/urls.py
@@ -1,9 +1,10 @@
+from ast import Pass
 from django.urls import path
 from . import views  # Import your views module
 
 urlpatterns = [
     path("<int:course_id>/", views.create_checkout_session, name="create_checkout_session"), 
-    path("course_success/<int:course_id>/", views.course_success, name="course_success"),
+    path("course_success/", views.course_success, name="course_success"),
     path("course_cancel/", views.course_cancel, name="course_cancel"),  # Added missing slash and corrected function
     path("stripe/webhook/", views.stripe_webhook, name="stripe_webhook"),  # Added missing slash
 ]
