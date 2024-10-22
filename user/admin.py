c4ba48a52f610c84221900e98d005d0a669a2104
arhamfareed106 <arhamfareed575@gmail.com>
2024-10-22 13:26:43 +0500

    add css

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+# Register the User model with the default UserAdmin
+admin.site.unregister(User)  # Unregister if it was registered previously
+admin.site.register(User, UserAdmin)
